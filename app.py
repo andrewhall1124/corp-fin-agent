f8c52f6bd6395a3e3b3ac62c9260dc83211b0d7a
andrewhall1124 <andrewmartinhall2@gmail.com>
2025-01-30 18:59:19 -0700

    Working streamlit demo.

--- app.py
+++ app.py
@@ -47,15 +47,28 @@ balance_sheet = {
     "term_loan": data["term_loan"].to_list(),
     "net_worth": data["net_worth"].to_list(),
 }
+st.set_page_config(layout="wide")
+st.title("Cashflow Conversion Cycle")
+
+col1, col2 = st.columns(2)
+
+with col1:
+
+    col1a, col1b = st.columns(2)
+
+    st.subheader("Assumptions", divider="gray")
+
+    with col1a:
+        sales_growth = st.number_input("Sales Growth (%)", value=25)
+
+    with col1b:
+        interest_rate = st.number_input("Interest Rate (%)", value=11)
 
 ccc = CashConversionCycle(
     income_statement=income_statement,
     balance_sheet=balance_sheet,
-    sales_growth=0.25,
-    interest_rate=0.11,
+    sales_growth=sales_growth / 100,
+    interest_rate=interest_rate / 100,
 )
 
-st.title("Cashflow Conversion Cycle")
-# Two assumption inputs.
-# Fix narrow formatting.
 st.table(ccc.to_df())
