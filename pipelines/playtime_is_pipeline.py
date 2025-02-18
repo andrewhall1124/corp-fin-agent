53427774a0a578705e5b5cb2e039350a22bf85e8
Chuck Nadauld <59513834+Chucknadauld@users.noreply.github.com>
2025-02-18 08:36:01 -0700

    Did the data pipeline for Play Time (#4)
    * Did the data pipeline for Play Time
    
    * Fixed all the things Andrew wanted me to.

---
+++ pipelines/playtime_is_pipeline.py
@@ -0,0 +1,75 @@
+import pandas as pd
+from database.database import Database
+
+COMPANY_ID = 2
+TABLE_NAME = "playtime_is"
+STG_TABLE = TABLE_NAME + "_stg"
+XF_TABLE = TABLE_NAME + "_xf"
+
+# Import raw
+df = pd.read_csv("data/playtime_is.csv")
+
+# Rename columns
+df["value"] = df["value"].str.lower()
+df["value"] = df["value"].str.replace(" ", "_")
+df = df.rename(columns={"value": "year"})
+
+# Set index
+df = df.set_index("year")
+
+# Transpose
+df = df.T.reset_index(names="year")
+
+# SQL Queries
+with Database() as db:
+    # Stage table
+    db.load_df(df=df, table=STG_TABLE, replace=True, index=False)
+
+    # Drop existing transform table
+    db.execute(f"DROP TABLE IF EXISTS {XF_TABLE};")
+
+    # Transform Table
+    db.execute(
+        f"""
+        CREATE TABLE {XF_TABLE} AS 
+            SELECT
+                {COMPANY_ID} AS company_id,
+                year,
+                net_sales,
+                cogs AS cost_of_goods_sold,
+                operating_expense,
+                interest_expense,
+                taxes
+            FROM {STG_TABLE}
+        ;
+        """
+    )
+
+    # Merge
+    db.execute(
+        f"""
+        INSERT OR REPLACE INTO income_statement(
+            company_id,
+            year,
+            net_sales,
+            cost_of_goods_sold,
+            operating_expense,
+            interest_expense,
+            taxes
+        )
+        SELECT
+            company_id,
+            year,
+            net_sales,
+            cost_of_goods_sold,
+            operating_expense,
+            interest_expense,
+            taxes
+        FROM {XF_TABLE}
+        ;
+        """
+    )
+
+    # Drop stage and transform tables
+    db.execute(f"DROP TABLE IF EXISTS {STG_TABLE};")
+    db.execute(f"DROP TABLE IF EXISTS {XF_TABLE};")
