d9118dcd1ae001e272e9ce9ae948a3432662bdc2
andrewhall1124 <andrewmartinhall2@gmail.com>
2025-02-19 14:12:21 -0700

    Updated balance sheet dataclass.

--- app/statements.py
+++ app/statements.py
@@ -5,15 +5,15 @@ from dataclasses import dataclass
 class BalanceSheet:
     year: int
     cash: float
-    accounts_recievable: float
-    inventory: float
-    property_plant_and_equipment: float
-    accounts_payable: float
-    short_term_debt: float
-    long_term_debt_current_portion: float
-    other_current_liabilities: float
-    long_term_debt: float
-    share_holders_equity: float
+    accounts_recievable: list[float]
+    inventory: list[float]
+    property_plant_and_equipment: list[float]
+    accounts_payable: list[float]
+    short_term_debt: list[float]
+    long_term_debt_current_portion: list[float]
+    other_current_liabilities: list[float]
+    long_term_debt: list[float]
+    share_holders_equity: list[float]
 
 
 @dataclass
