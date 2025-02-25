6de8d97034bed6c04ececc10859409d0d569758d
Charlie Nadauld <charlienadauld@gmail.com>
2025-02-25 12:53:58 -0700

    Added select company dropdown

--- app/app.py
+++ app/app.py
@@ -1,33 +1,31 @@
 import pandas as pd
 import streamlit as st
 import dao
-
 from cash_conversion_cycle import CashConversionCycle
 
-company_id = 1
-
-income_statement = dao.load_income_statement(company_id)
-balance_sheet = dao.load_balance_sheet(company_id)
-
-# make key assumptions / cash conversion cycle cells a darker color with white letters
-
 st.set_page_config(layout="wide", page_title="Cashflow Conversion Cycle")
-
 st.title("Cashflow Conversion Cycle")
 
 col1, col2 = st.columns(2)
 
 with col1:
-
-    col1a, col1b = st.columns(2)
-
-    st.subheader("Assumptions", divider="gray")
-
+    col1a, col1b, col1c = st.columns(3)
+    
+    company_options = {
+        "Clarkson": 1,
+        "Playtime": 2,
+    }
+    
     with col1a:
         sales_growth = st.number_input("Sales Growth (%)", value=25)
-
     with col1b:
         interest_rate = st.number_input("Interest Rate (%)", value=11)
+    with col1c:
+        selected_company = st.selectbox("Select Company", options=list(company_options.keys()))
+        company_id = company_options[selected_company]
+
+income_statement = dao.load_income_statement(company_id)
+balance_sheet = dao.load_balance_sheet(company_id)
 
 ccc = CashConversionCycle(
     income_statement,
@@ -42,13 +40,13 @@ df = ccc.to_df()
 def highlight_cells(df):
     highlighted = pd.DataFrame('', index=df.index, columns=df.columns)
     
-    # highlight the section headers with light gray, black text, and underline
+    # Highlight section headers with light gray, black text, and underline
     header_rows = [2, 7, 13, 27]
     for row in header_rows:
         for col in df.columns:
             highlighted.loc[row, col] = 'background-color: #666666; color: black; border-bottom: 2px solid black'
     
-    # highlight forecast cells with transparent yellow
+    # Highlight goal cells with amber
     for row in range(8, 12):
         for col in ['E', 'F', 'G', 'H']:
             highlighted.loc[row, col] = 'background-color: rgba(255, 193, 7, 0.3)'
